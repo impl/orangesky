917d73697db2ef493b070fe578913e6fb36f4348
Noah Fontes <nfontes@cynigram.com>
2015-09-01 13:12:03 -0700

    Rename src/arch to src/target.

--- build.rs
+++ build.rs
@@ -7,14 +7,14 @@ use std::path;
 fn main() {
     let target = env::var("TARGET").unwrap();
 
-    let arch_path = path::PathBuf::from("src/arch").join(&target);
-    if fs::metadata(&arch_path).is_err() {
+    let target_path = path::PathBuf::from("src/target").join(&target);
+    if fs::metadata(&target_path).is_err() {
         panic!("unsupported target: {}", target);
     }
 
     let mut config = gcc::Config::new();
     for file_name in ["start.S"].iter() {
-        let file_path = arch_path.join(file_name);
+        let file_path = target_path.join(file_name);
         config.file(file_path.as_os_str());
     }
     config.compile("libstart.a");
